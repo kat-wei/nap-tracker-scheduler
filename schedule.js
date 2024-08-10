fdb00bf8a16cc542d177c33670776ca220fe75f6
Katherine <katherine.wei7@gmail.comssh-keygen -t ed25519 -C katherine.wei7@gmail.comgit config --global user.name Katherine>
2024-08-10 09:44:55 -0600

    Initial commit

---
+++ schedule.js
@@ -0,0 +1,17 @@
+import './index.css';
+function Schedule() {
+    return(
+        <html>
+            <head>
+                <title> Nap Scheduler </title>
+            </head>
+
+            <div class="schedule">
+                implement this later
+            </div>  
+        </html>
+    );
+
+}
+
+return Schedule();
