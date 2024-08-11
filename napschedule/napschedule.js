ee6eead65fbcb272656876293ce113fa32b326c6
Katherine <katherine.wei7@gmail.comssh-keygen -t ed25519 -C katherine.wei7@gmail.comgit config --global user.name Katherine>
2024-08-11 15:53:40 -0600

    Local storage updated with wake and bed time

--- napschedule/napschedule.js
+++ napschedule/napschedule.js
@@ -1,27 +1,30 @@
 
 
 function saveTimes() {
-    // const wakeTime = document.getElementById('wake-time').value;
-    // const sleepTime = document.getElementById('sleep-time').value;
+    const wakeTime = document.getElementById('wake-time').value;
+    const sleepTime = document.getElementById('sleep-time').value;
     // console.log(wakeTime);
     // console.log(sleepTime);
 
-    // localStorage.setItem('wakeTime', wakeTime);
-    // localStorage.setItem('sleepTime', sleepTime);
-    window.location.href = "schedule.html";
+    localStorage.setItem('wakeTime', wakeTime);
+    localStorage.setItem('sleepTime', sleepTime);
+    
 }
+
+function loadTimes(){
+    const wakeTime = localStorage.getItem('wakeTime');
+    const sleepTime = localStorage.getItem('sleepTime');
+
+    console.log(wakeTime);
+    console.log(sleepTime);
+}
+
+saveTimes();
+loadTimes();
 
 // // Retrieve the saved times from localStorage
 // function loadTimes() {
 //     const wakeTime = localStorage.getItem('wakeTime');
 //     const sleepTime = localStorage.getItem('sleepTime');
-//     if (wakeTime !== null) {
-//         document.getElementById('wake-time').value = wakeTime;
-//     }
-//     if (sleepTime !== null) {
-//         document.getElementById('sleep-time').value = sleepTime;
-//     }
-// }
 
-// // Load the saved times when the page loads
-// window.onload = loadTimes;
+// }
